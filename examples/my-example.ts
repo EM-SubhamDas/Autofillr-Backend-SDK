3c801a0d8bb51dae8705f7b254902bfa067b56ae
Subham15113 <Subham.D@engineersmind.com>
2026-05-19 15:14:13 +0530

    feat: add my example

---
+++ examples/my-example.ts
@@ -0,0 +1,14 @@
+#!/usr/bin/env -S npm run tsn -T
+
+import PdffillrSDK from '@pdffillr/sdk';
+
+const client = new PdffillrSDK({ apiKey: process.env['PDFFILLR_API_KEY'] });
+
+async function main() {
+  const session = await client.sdk.chat.sessions.create({ 
+    title: 'Test session' 
+  });
+  console.log(session);
+}
+
+main();
